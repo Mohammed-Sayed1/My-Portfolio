81812037b3520c74834016ec31d6ca1b7bb49c6e
Mohammed Sayed <farsfhd123@gmail.com>
2023-05-07 05:36:38 +0300

    Header shadow on scroll is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -153,8 +153,14 @@ function scrollActive() {
   });
 }
 window.addEventListener("scroll", scrollActive);
-/*==================== CHANGE BACKGROUND HEADER ====================*/
 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  if (this.scrollY > 0) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
 /*==================== SHOW SCROLL UP ====================*/
 
 /*==================== DARK LIGHT THEME ====================*/
